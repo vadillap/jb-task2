7dd3e924cf31ccc7d1b04ab73149344ecee6cd74
Vadim <vadillamc@gmail.com>
2021-04-11 22:58:46 +0300

    add email generator

---
+++ email_gen/src/main.rs
@@ -0,0 +1,18 @@
+use rand::{SeedableRng, Rng};
+use rand_regex;
+use std::fs;
+
+fn main() {
+    let mut rng = rand_xorshift::XorShiftRng::from_seed(*b" Jet Brains ^_^ ");
+
+    let regex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+
+    let gen = rand_regex::Regex::compile(regex, 100).unwrap();
+
+    let samples = (&mut rng).sample_iter(&gen).take(100).collect::<Vec<String>>();
+
+    let samples = samples.join("\n");
+
+
+    fs::write("emails.txt", samples).expect("Unable to write file");
+}
